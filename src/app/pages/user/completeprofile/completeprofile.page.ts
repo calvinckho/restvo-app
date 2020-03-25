8108123ce03955eb82b99f4764c872b5582e7bd7
Calix Huang <calix.huang1@gmail.com>
2020-03-25 12:47:16 -0700

    changes for review on completeprofile

--- src/app/pages/user/completeprofile/completeprofile.page.ts
+++ src/app/pages/user/completeprofile/completeprofile.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-completeprofile',
   templateUrl: './completeprofile.page.html',
   styleUrls: ['./completeprofile.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CompleteprofilePage implements OnInit {
 
