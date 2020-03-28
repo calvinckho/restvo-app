ff95a83809c083f0fab732ee2eb8bbf7ac2f7f5b
calvinho <cho@wee.church>
2020-03-27 22:48:11 -0700

    Calvin's version of the Me page
    1.8.3 (1)

--- src/app/pages/user/completeprofile/completeprofile.page.ts
+++ src/app/pages/user/completeprofile/completeprofile.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {ModalController, Platform} from "@ionic/angular";
+import {Location} from "@angular/common";
+import {UserData} from "../../../services/user.service";
+import {Auth} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-completeprofile',
@@ -8,9 +12,16 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class CompleteprofilePage implements OnInit {
 
-  constructor() { }
+  @Input() modalPage: any;
+
+  constructor(
+      public location: Location,
+      public platform: Platform,
+      public modalCtrl: ModalController,
+      public userData: UserData,
+      public authService: Auth
+  ) { }
 
   ngOnInit() {
   }
-
 }
