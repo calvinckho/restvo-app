b09c26267db2d113b49c214ab97a29c5d73fbbd3
XINLU ZHOU <xiz261@ucsd.edu>
2020-05-01 18:49:39 -0700

    modalcontroller

--- src/app/pages/feature/showfeature/showfeature.page.ts
+++ src/app/pages/feature/showfeature/showfeature.page.ts
@@ -2099,7 +2099,7 @@ export class ShowfeaturePage implements OnInit, OnDestroy {
     }
 
   async openRegister(slide, loginStatus) {
-      const registerModal = await this.modalCtrl.create({component: RegisterPage, componentProps: { slide: slide, loginStatus: loginStatus, modalPage: true}} );
+      const registerModal = await this.modalCtrl.create({component: RegisterPage, componentProps: { slide: slide, loginStatus: loginStatus, modalPage: true}},{cssClass: "modal-fullscreen" });
       await registerModal.present();
   }
 
