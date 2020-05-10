2fe59cc41e0bebfc3331e519711d937a913f6c6a
Calvin Ho <ch235@cornell.edu>
2020-05-10 03:42:30 -0700

    improve logAppUsage in webapp and backend, now logging mobile app platform and app version data
    1.8.12 (2)

--- src/app/services/systemlog.service.ts
+++ src/app/services/systemlog.service.ts
@@ -3,22 +3,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Auth } from './auth.service';
 import { UserData } from './user.service';
 import { NetworkService } from './network-service.service';
+import { AppVersion } from "@ionic-native/app-version/ngx";
+import {Platform} from "@ionic/angular";
 
 @Injectable({ providedIn: 'root' })
 export class Systemlog {
 
     constructor(private http: HttpClient,
+                private platform: Platform,
+                private appVersion: AppVersion,
                 public authService: Auth,
                 public userData: UserData,
                 public networkService: NetworkService) {
     }
 
-    logAppUsage(time) {
-        let churches = [];
-        if (this.userData && this.userData.user && this.userData.user.churches && this.userData.user.churches.length) {
-            churches = this.userData.user.churches.map((c) => c._id);
+    async logAppUsage(time) {
+        const data: any = {topic: 'Mobile App Usage', time: time, appPlatforms: this.platform.platforms()};
+        if (this.userData && this.userData.defaultProgram && this.userData.defaultProgram._id) {
+            data.defaultProgram = this.userData.defaultProgram._id;
         }
-        return this.http.post(this.networkService.domain + '/api/systemlog/appusage', JSON.stringify({time: time, churches: churches}), this.authService.httpAuthOptions).toPromise();
+        if (this.platform.is('cordova')) {
+            data.appVersionNumber = await this.appVersion.getVersionNumber();
+        }
+        return this.http.post(this.networkService.domain + '/api/systemlog/appusage', JSON.stringify(data), this.authService.httpAuthOptions).toPromise();
     }
 }
 
