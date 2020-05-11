a1490ca4c450dd2fbb3e067bfe200f554b65f458
Calix Huang <calix.huang1@gmail.com>
2020-05-11 14:48:54 -0700

    removed outlet for chat routing

--- src/app/pages/feature/manage/managefeature.page.ts
+++ src/app/pages/feature/manage/managefeature.page.ts
@@ -214,7 +214,6 @@ export class ManagefeaturePage extends EditfeaturePage implements OnInit {
       if (this.platform.width() >= 768) {
           this.chatService.currentChatProps.push(chatObj);
           // when clicking on a conversation, if it is displaying the group info, it will force it to get back to the chat view
-          console.log(this.moment)
           this.router.navigate(['/app/manage/activity/' + this.moment._id + '/chat'], { skipLocationChange: true });
           // if it is displaying the chat view, it will reload the chat data
           this.userData.refreshMyConversations({action: 'reload chat view'});
