607861e8ac2ab288278e9770202964c9e6e1a95a
Calvin Ho <ch235@cornell.edu>
2020-05-13 13:48:34 -0700

    improving calix's code
    1.8.12 (3)

--- src/app/pages/feature/manage/feature-childactivities/feature-childactivities.module.ts
+++ src/app/pages/feature/manage/feature-childactivities/feature-childactivities.module.ts
@@ -11,8 +11,14 @@ import {ApplicationPipesModule} from "../../../../pipes/application-pipes";
 const routes: Routes = [
   {
     path: '',
-    component: FeatureChildActivitiesPage
-  }
+    component: FeatureChildActivitiesPage,
+    children: [
+      {
+        path: 'chat',
+        loadChildren: '../../group/groupchat/groupchat.module#GroupchatPageModule',
+      },
+    ]
+  },
 ];
 
 @NgModule({
