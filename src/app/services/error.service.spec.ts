f5411c728e237cd4fd91ed4288b4808673f85dcc
Lorne Zhang <lorneez@lornes-mbp.lan>
2020-07-29 13:53:10 -0700

    Added Error service and added it into the app.module. Error service catches HttpErrorResponse errors'

--- src/app/services/error.service.spec.ts
+++ src/app/services/error.service.spec.ts
@@ -7,7 +7,7 @@ describe('ErrorService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.get(ErrorService); // Tutorial says to use TestBed.inject, but angular version may not be up to date.
+    service = TestBed.inject(ErrorService); // Tutorial says to use TestBed.inject, but angular version may not be up to date.
   });
 
   it('should be created', () => {
