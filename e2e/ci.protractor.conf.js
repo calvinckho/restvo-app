5ea96bd8c5af2e495184421bfdc4efcc99eba599
Calvin Ho <ch235@cornell.edu>
2020-08-11 21:10:01 -0700

    adding Travis CI settings

--- e2e/ci.protractor.conf.js
+++ e2e/ci.protractor.conf.js
@@ -1,6 +1,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+//const { SpecReporter } = require('jasmine-spec-reporter');
+
 exports.config = {
   allScriptsTimeout: 110000,
   specs: [
@@ -21,5 +23,11 @@ exports.config = {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    //jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
