e4fbb9ce92d158556e64e7afbdb29dbf3865d27c
Kevin Tolentino <kevin.tolentino95@gmail.com>
2020-10-07 09:52:21 -0700

    added initial files for e2e test and added debug.log to git ignore due to vs code bug

---
+++ e2e/src/add-and-remove-restvo-friend.e2e-spec.ts
@@ -0,0 +1,71 @@
+import {
+  AppPage,
+  MaintabPage,
+  RegisterPage,
+  NewsPage,
+  ChatPage,
+  DiscoverPage,
+  DashboardPage,
+  ShowfeaturePage,
+  PickfeaturePopoverPage,
+  PickpeoplePopoverPage,
+  OnboardingfeaturePage,
+  CreateFeaturePage,
+  AboutPage,
+  PreferencesPage
+} from './app.po';
+import { browser } from 'protractor';
+
+describe('join journey and leave journey', () => {
+  let app: AppPage;
+  let showfeature: ShowfeaturePage;
+  let maintab: MaintabPage;
+  let register: RegisterPage;
+  let news: NewsPage;
+  let chat: ChatPage;
+  let discover: DiscoverPage;
+  let about: AboutPage;
+  let preferences: PreferencesPage;
+  let me: DashboardPage;
+  let pickfeature: PickfeaturePopoverPage;
+  let pickpeople: PickpeoplePopoverPage;
+  let createfeature: CreateFeaturePage;
+  let onboardfeature: OnboardingfeaturePage;
+
+  beforeAll(async () => {
+    // testing on desktop sized screen
+    const width = 1200;
+    const height = 968;
+    await browser.driver.manage().window().setSize(width, height);
+    app = new AppPage();
+    maintab = new MaintabPage();
+    register = new RegisterPage();
+    showfeature = new ShowfeaturePage();
+    news = new NewsPage();
+    chat = new ChatPage();
+    discover = new DiscoverPage();
+    me = new DashboardPage();
+    about = new AboutPage();
+    preferences = new PreferencesPage();
+    pickfeature = new PickfeaturePopoverPage();
+    pickpeople = new PickpeoplePopoverPage();
+    createfeature = new CreateFeaturePage();
+    onboardfeature = new OnboardingfeaturePage();
+    await browser.get('/app/activity/5ed1aafcb257a55e9c25beea;type=2;token=ZcksTu5LiY');
+  });
+
+  it('should show unauthenticated journey page', async () => {
+    await showfeature.waitUntilElementPresent('#showfeature-header');
+    expect(await showfeature.headerIsPresent('#showfeature-header')).toBeTruthy();
+  });
+
+  it('should login and show authenticated journey page', async () => {
+    await showfeature.clickElement('#accept-invitation');
+    await register.waitUntilVisible();
+    await register.fillEmail();
+    await register.fillPassword();
+    await register.submitLoginForm();
+    await register.waitUntilInvisible();
+    expect(await maintab.waitUntilPresent()).toBeTruthy();
+  });
+});
