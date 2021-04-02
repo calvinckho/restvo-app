d6f1f893584bac098d50d0b00c64820e245820ca
Calvin Ho <ch235@cornell.edu>
2021-04-01 19:06:46 -0700

    try to improve ci test

--- e2e/src/desktop/add-and-remove-restvo-friend.e2e-spec.ts
+++ e2e/src/desktop/add-and-remove-restvo-friend.e2e-spec.ts
@@ -84,11 +84,10 @@ describe(' Add and Remove a Restvo User as friend', () => {
   });
 
   it('should select Asia Ho and show confirmation alert', async () => {
-    await app.waitUntilElementVisible('#chatSearchBar');
     await app.enterNonRegistrationInputText('#chatSearchBar', 'Asia Ho', '.searchbar-input');
-    await browser.sleep(3000); // wait to ensure intended user shows up on list
+    await browser.sleep(4000); // wait to ensure intended user shows up on list
     await app.clickElement('#restvoGroup ion-item');
-    await browser.sleep(1000); // this is needed to prevent the race condition on circle.ci
+    await browser.sleep(1500); // this is needed to prevent the race condition on circle.ci
     await app.clickElement('#selectAppUsersButton');
     await app.waitUntilElementVisible('ion-alert');
     expect(await app.elementIsPresent('ion-alert')).toBeTruthy(); // expect confirmation alert to be truthy
