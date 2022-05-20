83fe74353af9a36dcc55543148d485cdadd3cca8
calvinho <cho@wee.church>
2022-05-19 21:02:47 -0700

    add PDF and Microsoft Document types, update README.md with full installation instructions.

--- android/app/src/main/java/com/restvo/app/MainActivity.java
+++ android/app/src/main/java/com/restvo/app/MainActivity.java
@@ -1,28 +1,10 @@
 package com.restvo.app;
-
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.capacitor.jitsi.plugin.Jitsi;
-import com.capacitor.shareextension.plugin.ShareExtension;
-//import com.byteowls.capacitor.oauth2.OAuth2ClientPlugin;
 
 import android.content.Intent;
 import android.webkit.ValueCallback;
 
-
-import java.util.ArrayList;
-
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    registerPlugin(Jitsi.class);
-    registerPlugin(ShareExtension.class);
-  }
-
   @Override
   protected void onNewIntent(Intent intent) {
     super.onNewIntent(intent);
@@ -37,5 +19,4 @@ public class MainActivity extends BridgeActivity {
       });
     }
   }
-
 }
