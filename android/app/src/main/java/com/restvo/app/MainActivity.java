15c01c0c7c5cba24bfcca7c987b94bcbe34a9591
calvinho <cho@wee.church>
2022-09-17 12:00:36 -0700

    app-dev01 branch adapted for Capacitor 4

--- android/app/src/main/java/com/restvo/app/MainActivity.java
+++ android/app/src/main/java/com/restvo/app/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 import com.capacitor.jitsi.plugin.Jitsi;
-import com.capacitor.shareextension.plugin.ShareExtension;
+//import com.capacitor.shareextension.plugin.ShareExtension;
 //import com.byteowls.capacitor.oauth2.OAuth2ClientPlugin;
 
 import java.util.ArrayList;
@@ -16,6 +16,6 @@ public class MainActivity extends BridgeActivity {
     super.onCreate(savedInstanceState);
 
     registerPlugin(Jitsi.class);
-    registerPlugin(ShareExtension.class);
+    //registerPlugin(ShareExtension.class);
   }
 }
